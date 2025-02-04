babe7357243769fe3f4b99374430c3296710f6fb
Santos Enoque <santosenoque.ss@gmail.com>
2025-02-05 00:04:56 +0900

    add tailwind

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "../styles/global.css";
 import "./popup.css";
 
 const App = () => {
   return (
-    <div>
-      <img src="icon.png" alt="icon" />
+    <div className="w-[300px] h-[300px] bg-blue-500">
+      <div className="p-4">
+        <img src="icon.png" alt="icon" className="w-16 h-16" />
+      </div>
     </div>
   );
 };
