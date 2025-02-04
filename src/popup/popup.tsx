17e5c2992e28f755df8ebc74cac2d8c8bee4edcf
Santos Enoque <santosenoque.ss@gmail.com>
2025-02-05 00:33:50 +0900

    add shadcn support

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Button } from "../components/ui/button";
 import "../styles/global.css";
 import "./popup.css";
 
 const App = () => {
   return (
-    <div className="w-[300px] h-[300px] bg-blue-500">
-      <div className="p-4">
+    <div className="w-[300px] h-[300px] bg-background p-4">
+      <div className="space-y-4">
         <img src="icon.png" alt="icon" className="w-16 h-16" />
+        <Button>Click me</Button>
+        <Button variant="secondary">Secondary Button</Button>
+        <Button variant="destructive">Destructive Button</Button>
       </div>
     </div>
   );
